e3d897b6273dad91572752e002cfe34fa973bdb8
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2023-05-23 22:18:09 +0100

    Added inner shadow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20 shadow-[0_2px_0_]"
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
