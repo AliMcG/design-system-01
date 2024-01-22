7516f34497ed2d3536bd795ed5aef5938befe1b1
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-01-22 20:50:38 +0000

    Add Storybook

---
+++ src/components/Loader.tsx
@@ -0,0 +1,18 @@
+import { CogIcon, Cog8ToothIcon } from '@heroicons/react/24/solid'
+
+
+const Loader = () => {
+    return (
+        <>
+
+ <CogIcon className='h-8 w-8 animate-spin'/>
+
+            
+
+        <p>Loading...</p>
+        </>
+        
+    )
+}
+
+export default Loader
